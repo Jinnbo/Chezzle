46449cc721affd90b1dfd8b065fc9071017e5572
Jimmy <jimmyhui92012@gmail.com>
2023-02-04 22:00:25 -0800

    Create puzzle schema

--- src/models/Puzzle.js
+++ src/models/Puzzle.js
@@ -1,11 +1,17 @@
 const mongoose = require('mongoose')
 
 const puzzleSchema = new mongoose.Schema({
+    PuzzleId: String,
     FEN: String,
-    moves: String,
-    ratings: String,
-    themes: String,
-    opening: String
+    Moves: String,
+    Rating: String,
+    RatingDeviation: String,
+    Popularity: String,
+    NbPlays: String,
+    Themes: String,
+    GameUrl: String,
+    OpeningFamily: String,
+    OpeningVariation: String
 })
 
 module.exports = mongoose.model("Puzzles", puzzleSchema)
--- src/server.js
+++ src/server.js
@@ -4,7 +4,25 @@ const mongoose = require("mongoose");
 
 const Puzzle = require('./models/Puzzle')
 
+mongoose.set("strictQuery", false);
 mongoose.connect(
-    'mongodb+srv://Jimmy:8dGDYmEBj7AMECoI@cluster0.tn1aa6o.mongodb.net/?retryWrites=true&w=majority',
-    () => {console.log("Successfully Connected to mongoDB")}
-);
+    'mongodb+srv://Jimmy:8dGDYmEBj7AMECoI@cluster0.tn1aa6o.mongodb.net/Puzzles',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("Connected to MongoDB")
+    })
+    .catch((err) => console.log(err))
+
+async function run(){
+
+    const puzzle = new Puzzle({
+
+        
+    })
+    await puzzle.save()
+    console.log(puzzle )
+}
+
