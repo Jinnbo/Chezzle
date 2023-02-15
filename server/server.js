1178ccf6ff009c180faa8d5297da6972bdf61708
Jimmy <jimmyhui92012@gmail.com>
2023-02-14 23:36:01 -0800

    added mates

--- server/server.js
+++ server/server.js
@@ -1,11 +1,31 @@
 const express = require('express');
-const app = express();
 const mongoose = require("mongoose");
 
 const Puzzle = require('./models/Puzzle');
 
-mongoose.set("strictQuery", false);
-mongoose.connect(
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+app.get('/puzzles/mateIn2', async (req, res) => {
+    try{
+        const result = await Puzzle.find(
+            { Themes: {$regex: /^mate/ } }
+        );
+        res.send(result);
+    }catch(e){
+        res.status(500).send({message: e.message});
+    }
+});
+
+
+
+app.listen(3000, async () => {
+    mongoose.set("strictQuery",false);
+    mongoose.connect(
     'mongodb+srv://Jimmy:8dGDYmEBj7AMECoI@cluster0.tn1aa6o.mongodb.net/Puzzles',
     {
         useNewUrlParser: true,
@@ -15,41 +35,4 @@ mongoose.connect(
         console.log("Connected to MongoDB")
     })
     .catch((err) => console.log(err))
-
-const csv = require('csv-parser');
-const fs = require('fs');
-const results = [];
-
-fs.createReadStream('./assets/lichessPuzzles.csv')
-    .pipe(csv({}))
-    .on('data', (data) => results.push(data))
-    .on('end', async () => {
-        
-        var count = 1;
-
-        // Iterate through results and store it as a Puzzle
-        for (var i=0;i<results.length;i++){
-            const puzzle = new Puzzle({
-                PuzzleId: results[i].PuzzleId,
-                FEN: results[i].FEN,
-                Moves: results[i].Moves,
-                Rating: results[i].Rating,
-                RatingDeviation: results[i].RatingDeviation,
-                Popularity: results[i].Popularity,
-                NbPlays: results[i].NbPlays,
-                Themes: results[i].Themes,
-                GameUrl: results[i].GameUrl,
-                OpeningFamily: results[i].OpeningFamily,
-                OpeningVariation: results[i].OpeningVariation 
-            })
-            await puzzle.save();
-            if (i % 10000 == 0){
-                console.log("Percent: " + count);
-                count++;
-                
-            }
-        }
-    });
-
-
-
+})
