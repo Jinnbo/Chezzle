b65576c09ea508c723ebb61a537176deb110d514
Jimmy <jimmyhui92012@gmail.com>
2023-03-31 01:06:50 -0700

    Undo and forward btn

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,11 @@ import Board from "./components/Board";
 function App() {
   return (
     <>  
-      
       <Board></Board>
     </>
   );
 }
+
+document.body.style = 'background: rgb(33,37,41);';
 
 export default App;
--- client/src/components/Board.js
+++ client/src/components/Board.js
@@ -4,6 +4,16 @@ import { Chessboard } from "react-chessboard";
 import { mateService } from '../services/mate.service';
 import './Board.css';
 
+/*
+TODO
+- Add sound effects for all moves
+- Add the undo btn
+
+LOGIC
+- Display modal when puzzle is solved
+
+*/
+
 export default function Board(){    
 
     // Game states
@@ -19,13 +29,14 @@ export default function Board(){
     
     useEffect(()=>{
         if (game.inCheck()) checkSound.play()
-        //else moveSound.play()
+        else moveSound.play()
 
-        /*
-        
-        
-        
-        */
+        if (game.isCheckmate()){
+            window.onload = setTimeout(function(){
+                alert('Complete');
+                setPuzzle();
+            }, 1000); 
+        }
 
     },[game])
 
@@ -150,8 +161,20 @@ export default function Board(){
 
     return (
         <>  
-            <div class="randomBTNContainer">
-               <button class="btn btn-primary btn-lg" onClick={setPuzzle}> Random Puzzle </button>
+            <div className='topBTNS'>
+                <div class="randomBTNContainer">
+                   <button class="btn btn-primary btn-lg" onClick={setPuzzle}> Random Puzzle </button>
+                </div>
+                <div className="undoBTN">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
+                    </svg>
+                </div>
+                <div className="forwardBTN">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
+                    </svg>
+                </div>
             </div>
             <div class='boardContainer'>
                 <Chessboard
