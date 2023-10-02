954d8253d49619b21ad5d88cd13dd7e3903552c0
Jimmy <jimmyhui92012@gmail.com>
2023-10-01 23:40:45 -0700

    Finished right box

--- client/src/components/RightBox/RightBox.jsx
+++ client/src/components/RightBox/RightBox.jsx
@@ -1,16 +1,31 @@
 import React, {Component,useState, useEffect} from 'react';
+import './RightBox.css'
 
 export default function RightBox(){
     return (
         <>
             <div className='flex flex-col justify-around w-[20rem] items-center bg-black rounded-3xl'>
-                <div className="">
+                <div className="text-white text-4xl font-bold">
                     Mate in 1
                 </div>
 
-                <div className="">
-                    <img src='./logo.png'>
-                    </img>
+                <div className="flex justify-center items-center gap-[2rem] w-[5rem] h-[5rem]">
+                    <img src='./correct.png'/>
+                    <div className="correctScore text-white text-4xl">0</div>
+                </div>
+                <div className="flex justify-center items-center gap-[2rem] w-[5rem] h-[5rem]">
+                    <img src='./incorrect.png'/>
+                    <div className="incorrectScore text-white text-4xl">0</div>
+                </div>
+
+                <div className="flex justify-between w-[16rem]">
+                    <img className='buttons w-[4rem] h-[2.5rem]' src='./backward.png'/>
+                    <img className='buttons w-[4rem] h-[2.5rem]' src='./hint.png'/>
+                    <img className='buttons w-[4rem] h-[2.5rem]' src='./forward.png'/>
+                </div>
+
+                <div className="restart flex justify-center rounded-md w-[16rem] h-[2.5rem] text-[1.5rem]">
+                    Restart
                 </div>
             </div>
         </>
